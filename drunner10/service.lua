b87fadc8767ba3b4c9ee79caf45f594a36c237ef
j842 <j@842.be>
2017-02-25 13:17:34 +1300

    use valid lua

--- drunner10/service.lua
+++ drunner10/service.lua
@@ -28,8 +28,8 @@ function start_mongo()
     "--smallfiles",
     "--oplogSize","128",
     "--replSet","rs0")
-    result or die("Failed to start mongodb : "..output)
-    dockerwait(dbcontainer, "27017") or die("Mongodb didn't respond on port 27017 in the expected timeframe.")
+    dieunless( result, "Failed to start mongodb : "..output)
+    dieunless( dockerwait(dbcontainer, "27017"), "Mongodb didn't respond on port 27017 in the expected timeframe.")
 
     -- run the mongo replica config
     result=docker("run","--rm",
@@ -38,7 +38,7 @@ function start_mongo()
     "mongo",dbcontainer .. "/rocketchat","--eval",
     "rs.initiate({ _id: 'rs0', members: [ { _id: 0, host: 'localhost:27017' } ]})"
     )
-    result or die("Mongodb replica init failed")
+    dieunless(result, "Mongodb replica init failed")
 end
 
 function start_rocketchat()
@@ -50,8 +50,8 @@ function start_rocketchat()
     "--env","MONGO_OPLOG_URL=mongodb://" .. dbcontainer .. ":27017/local",
     "-d","rocket.chat")
 
-    result or die("Failed to start rocketchat on port ${PORT} : "..output)
-    dockerwait(rccontainer, "3000") or die("Rocketchat didn't respond in the expected timeframe.")
+    dieunless(result, "Failed to start rocketchat on port ${PORT} : "..output)
+    dieunless(dockerwait(rccontainer, "3000"), "Rocketchat didn't respond in the expected timeframe.")
 end
 
 function start()
@@ -62,11 +62,11 @@ function start()
       
    -- use dRunner's built-in proxy to expose rocket.chat over SSL (port 443) on host.
    -- disable timeouts because rocket.chat keeps websockets open for ages.
-   proxyenable(sDomain,rccontainer,3000,network,sEmail,sMode,false) or die("Couldn't enable proxy")
+   dieunless( proxyenable(sDomain,rccontainer,3000,network,sEmail,sMode,false), "Couldn't enable proxy")
 end
 
 function stop()
-   proxydisable() or print("Couldn't disable proxy")
+   if not proxydisable() then print("Couldn't disable proxy") end
 
    dockerstop(rccontainer)
    dockerstop(dbcontainer)
@@ -74,31 +74,30 @@ end
 
 function uninstall()
    stop()
-   docker("network","rm",network) or print("Unable to remove network")
+   msgunless( docker("network","rm",network), "Unable to remove network") end
    -- we retain the database volume
 end
 
 function obliterate()
    stop()
-   docker("network","rm",network) or print("Unable to remove network")
-   dockerdeletevolume(dbvolume) or print("Unable to remove docker volume "..dbvolume)
+   msgunless( docker("network","rm",network), "Unable to remove network")
+   msgunless( dockerdeletevolume(dbvolume), "Unable to remove docker volume "..dbvolume)
 end
 
 -- install
 function install()
    dockerpull("mongo:3.2")
    dockerpull("rocket.chat")
-   dockercreatevolume(dbvolume) or die("Couldn't create docker volume "..dbvolume)
+   dieunless( dockercreatevolume(dbvolume), "Couldn't create docker volume "..dbvolume)
    result,output = docker("network","create",network)
-   result or die("Couldn't create network "..network.." : "..output)
+   dieunless( result, "Couldn't create network "..network.." : "..output)
 end
 
 function backup()
    docker("pause",rccontainer)
    docker("pause",dbcontainer)
 
-GAHHH THIS RETURNS A CRESULT NOT TRUE/FALSE.
-   dockerbackup(dbvolume)
+   dieunless( dockerbackup(dbvolume), "Failed to backup "..dbvolume )
 
    docker("unpause",dbcontainer)
    docker("unpause",rccontainer)
@@ -107,10 +106,10 @@ end
 function restore()
    dockerpull("mongo:3.2")
    dockerpull("rocket.chat")
-   dockerrestore(dbvolume)
+   dieunless( dockerrestore(dbvolume), "Couldn't restore "..dbvolume)
 
    result,output = docker("network","create",network)
-   result or die("Couldn't create network "..network.." : "..output)
+   dieunless(result, "Couldn't create network "..network.." : "..output)
 
 -- set mode to fake for safety!
    setconfig("MODE","fake")
